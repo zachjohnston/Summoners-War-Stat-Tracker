9a7c68b60eedc5ccf0af98981ac04bd651f3d5b4
Zachary Johnston <zacharyjohnston33@gmail.com>
2024-02-12 14:47:30 -0800

    Added try-except case to fix issues with values

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pytesseract as pt
 import numpy as np
 import pandas as pd
 import os
+import xlsxwriter
 
 directory = os.fsencode("images/")
 unit_list = [] #list
@@ -37,40 +38,48 @@ for file in os.listdir(directory):
 
     #put information in a tuple
     unitstats = ()
-    unitstats = unitstats + (textlist[0],)
+    unitstats = unitstats + (textlist[0],) #name
     for i in range(len(textlist)):
         match textlist[i]:
             case "Level":
-                unitstats = unitstats + (textlist[i+1],)
-                unitstats = unitstats + (textlist[i+3],)
-                unitstats = unitstats + (textlist[i+4],)
+                try:
+                    int(textlist[i+1]) #checks if it's 40 (int) or 40/40 (str)
+                except: #read the value wrong
+                    lvl = textlist[i+1].split('/') 
+                    unitstats = unitstats + (lvl[0],)
+                    unitstats = unitstats + (lvl[1],)
+                    unitstats = unitstats + (textlist[i+2],) 
+                else: #read the value correctly   
+                    unitstats = unitstats + (textlist[i+1],) #current level
+                    unitstats = unitstats + (textlist[i+3],) #max level
+                    unitstats = unitstats + (textlist[i+4],) #type
             case "HP":
-                unitstats = unitstats + (textlist[i+1],)
-                unitstats = unitstats + (textlist[i+2].strip('+'),)
+                unitstats = unitstats + (textlist[i+1],) #hp
+                unitstats = unitstats + (textlist[i+2].strip('+'),) #+hp
             case "ATK":
-                unitstats = unitstats + (textlist[i+1],)
-                unitstats = unitstats + (textlist[i+2].strip('+'),)
+                unitstats = unitstats + (textlist[i+1],) #atk
+                unitstats = unitstats + (textlist[i+2].strip('+'),) #+atk
             case "DEF":
-                unitstats = unitstats + (textlist[i+1],)
-                unitstats = unitstats + (textlist[i+2].strip('+'),)
+                unitstats = unitstats + (textlist[i+1],) #def
+                unitstats = unitstats + (textlist[i+2].strip('+'),) #+def
             case "SPD":
-                unitstats = unitstats + (textlist[i+1],)
-                unitstats = unitstats + (textlist[i+2].strip('+'),)
+                unitstats = unitstats + (textlist[i+1],) #spd
+                unitstats = unitstats + (textlist[i+2].strip('+'),) #+spd
             case "CRI":
                 if textlist[i+1] == "Rate":
-                    unitstats = unitstats + (textlist[i+2],)
+                    unitstats = unitstats + (textlist[i+2],) #cri rate
                 elif textlist[i+1] == "Dmg":
-                    unitstats = unitstats + (textlist[i+2],)
-                else: #probably default to cri dmg as it seems to have issues
-                    unitstats = unitstats + (textlist[i+1],)
+                    unitstats = unitstats + (textlist[i+2],) #cri dmg
             case "Resistance":
-                unitstats = unitstats + (textlist[i+1],)
+                unitstats = unitstats + (textlist[i+1],) #resistance
             case "Accuracy":
-                unitstats = unitstats + (textlist[i+1],)
+                unitstats = unitstats + (textlist[i+1],) #accuracy
     #add unit to full list
+    # print(unitstats)
     unit_list.append(unitstats)
 
-title_list = [
+#titles of the columns in sheet 1
+title_list_1 = [
     "Name",         #0          
     "Current Level",#1
     "Max Level",    #2
@@ -89,13 +98,27 @@ title_list = [
     "Accuracy"      #15
 ]
 
+#titles of the columns in sheet 2
+title_list_2 = [
+    "Name",
+    "Total HP"#,
+#    "Total ATK",
+#    "Total DEF",
+#    "Total SPD",
+#    "Total CRI Rate",
+#    "Total CRI Dmg",
+#    "Effective HP"
+]
+
 #print out the info
 # for i in range(len(unit_list)):
 #     print("-----------------")
 #     for j in range(len(unit_list[i])):
 #         print(str(title_list[j]) + ": " + str(unit_list[i][j]))
 
-final_data = pd.DataFrame.from_records(unit_list,columns=title_list)
-print(final_data)
+final_data_1 = pd.DataFrame.from_records(unit_list,columns=title_list_1)
+print(final_data_1)
 
-final_data.to_excel("data.xlsx", sheet_name="units",index=False)
+final_data_1.to_excel("data.xlsx", sheet_name="units",index=False)
+
+#want another sheet that has total hp, atk, def, spd, cri dmg, EHP with towers factored in
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
-# Description
-App that reads screen data in order to put your Summoner's War unit stats into a spreadsheet automatically.
-
 ## To Do List:
 - [X] Image recognition to extract the text
 - [X] Organization of the text
 - [ ] Have it read the screen data
 - [X] Put the data into a spreadsheet
 - [ ] Create an application
+
+## Current Issues
+- Certain data points are read incorrectly
